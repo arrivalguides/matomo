9d6fbb6105ed37d024ad0d6253fb421daba384a6
oleksiy.zubovskyy <bashconsole@gmail.com>
2018-09-06 12:46:19 +0300

    bugfix

--- piwik.php
+++ piwik.php
@@ -201,98 +201,97 @@ if(isset($_GET['idsite']) && $_GET['idsite'] == 1){
 
     $SITE_NAME = "";
 
-    if(count($parts) == 8 && $parts[2] == "www.arrivalguides.com" && $parts[4] == "Travelguide" && isset($parts[5])){
+// 1)
+    if(empty($SITE_NAME) && count($parts) == 8 && $parts[2] == "www.arrivalguides.com" && $parts[4] == "Travelguide" && isset($parts[5])){
        $SITE_NAME = $parts[5];
        if(isset($destination[strtoupper($SITE_NAME)])){
            $_GET['idsite'] = $destination[strtoupper($SITE_NAME)];
        }else file_put_contents("front.log", strtoupper($SITE_NAME) . " not found\n", FILE_APPEND);
     }
 
-    if(count($parts) == 5 && $parts[2] == "cmwidget.arrivalguides.com" && $parts[3] == "widget" && isset($parts[4])){
+//  2)
+    if(empty($SITE_NAME) && count($parts) == 5 && $parts[2] == "cmwidget.arrivalguides.com" && $parts[3] == "widget" && isset($parts[4])){
        $arr = explode("&", $parts[4]);
        if(isset($arr[1])){
           preg_match("/^iso=(.*)/i", $arr[1], $matches);
           $SITE_NAME = (isset($matches[1])) ? $matches[1] : "";
        }
-
        if(!empty($SITE_NAME) && isset($destination[strtoupper($SITE_NAME)])){
            $_GET['idsite'] = $destination[strtoupper($SITE_NAME)];
        }else file_put_contents("front.log", strtoupper($SITE_NAME) . " not found\n", FILE_APPEND);
     }
 
-
-    if(count($parts) == 5 && $parts[2] == "widget.arrivalguides.com" && $parts[3] == "widget" && isset($parts[4])){
+// 3)
+    if(empty($SITE_NAME) && count($parts) == 5 && $parts[2] == "widget.arrivalguides.com" && $parts[3] == "widget" && isset($parts[4])){
        $arr = explode("&", $parts[4]);
        if(isset($arr[1])){
           preg_match("/^iso=(.*)/i", $arr[1], $matches);
           $SITE_NAME = (isset($matches[1])) ? $matches[1] : "";
        }
-
        if(!empty($SITE_NAME) && isset($destination[strtoupper($SITE_NAME)])){
            $_GET['idsite'] = $destination[strtoupper($SITE_NAME)];
        }else file_put_contents("front.log", strtoupper($SITE_NAME) . " not found\n", FILE_APPEND);
     }
 
-
-    if(count($parts) == 8 && $parts[2] == "www.arrivalguides.com" && $parts[4] == "Travelguides" && isset($parts[7])){
+// 4)
+    if(empty($SITE_NAME) && count($parts) == 8 && $parts[2] == "www.arrivalguides.com" && $parts[4] == "Travelguides" && isset($parts[7])){
        $SITE_NAME = $parts[7];
        if(isset($destination[strtoupper($SITE_NAME)])){
            $_GET['idsite'] = $destination[strtoupper($SITE_NAME)];
        }else file_put_contents("front.log", strtoupper($SITE_NAME) . " not found\n", FILE_APPEND);
     }
 
-    if(count($parts) == 9 && $parts[2] == "www.arrivalguides.com" && $parts[4] == "Travelguides" && isset($parts[7])){
+// 5)
+    if(empty($SITE_NAME) && count($parts) == 9 && $parts[2] == "www.arrivalguides.com" && $parts[4] == "Travelguides" && isset($parts[7])){
        $SITE_NAME = $parts[7];
        if(isset($destination[strtoupper($SITE_NAME)])){
            $_GET['idsite'] = $destination[strtoupper($SITE_NAME)];
        }else file_put_contents("front.log", strtoupper($SITE_NAME) . " not found\n", FILE_APPEND);
     }
 
-
-    if(count($parts) == 8 && $parts[2] == "www.arrivalguides.com" && $parts[4] == "Partner" && $parts[6] == "Destination" && isset($parts[7])){
+// 6)
+    if(empty($SITE_NAME) && count($parts) == 8 && $parts[2] == "www.arrivalguides.com" && $parts[4] == "Partner" && $parts[6] == "Destination" && isset($parts[7])){
        $arr = explode("&", $parts[7]);
 file_put_contents("destinations.log", print_r($arr, true), FILE_APPEND);
        if(isset($arr[0])){
           preg_match("/^EmailDownload\?destination=(.*)/i", $arr[0], $matches);
           $SITE_NAME = (isset($matches[1])) ? $matches[1] : "";
        }
-
        if(!empty($SITE_NAME) && isset($destination[strtoupper($SITE_NAME)])){
            $_GET['idsite'] = $destination[strtoupper($SITE_NAME)];
        }else file_put_contents("front.log", strtoupper($SITE_NAME) . " not found\n", FILE_APPEND);
     }
 
-
-    if(count($parts) == 6 && $parts[2] == "www.arrivalguides.com" && $parts[4] == "Destination" && isset($parts[5])){
+// 7)
+    if(empty($SITE_NAME) && count($parts) == 6 && $parts[2] == "www.arrivalguides.com" && $parts[4] == "Destination" && isset($parts[5])){
        $arr = explode("&", $parts[5]);
        if(isset($arr[1])){
           preg_match("/^destination=(.*)/i", $arr[1], $matches);
           $SITE_NAME = (isset($matches[1])) ? $matches[1] : "";
        }
-
        if(!empty($SITE_NAME) && isset($destination[strtoupper($SITE_NAME)])){
            $_GET['idsite'] = $destination[strtoupper($SITE_NAME)];
        }else file_put_contents("front.log", strtoupper($SITE_NAME) . " not found\n", FILE_APPEND);
     }
 
-    if(count($parts) == 8 && $parts[2] == "www.arrivalguides.com" && $parts[4] == "Partner" && $parts[6] == "Destination" && isset($parts[7])){
+// 8)
+    if(empty($SITE_NAME) && count($parts) == 8 && $parts[2] == "www.arrivalguides.com" && $parts[4] == "Partner" && $parts[6] == "Destination" && isset($parts[7])){
        preg_match("/^Download\?destination=(.*)/i", $parts[7], $matches);
        $SITE_NAME = (isset($matches[1])) ? $matches[1] : "";
-
        if(!empty($SITE_NAME) && isset($destination[strtoupper($SITE_NAME)])){
            $_GET['idsite'] = $destination[strtoupper($SITE_NAME)];
        }else file_put_contents("front.log", strtoupper($SITE_NAME) . " not found\n", FILE_APPEND);
     }
 
-
-    if(count($parts) == 6 && $parts[2] == "www.arrivalguides.com" && $parts[4] == "Travelguide" && isset($parts[5])){
+// 9)
+    if(empty($SITE_NAME) && count($parts) == 6 && $parts[2] == "www.arrivalguides.com" && $parts[4] == "Travelguide" && isset($parts[5])){
        $SITE_NAME = $parts[5];
-
        if(isset($destination[strtoupper($SITE_NAME)])){
            $_GET['idsite'] = $destination[strtoupper($SITE_NAME)];
        }else file_put_contents("front.log", strtoupper($SITE_NAME) . " not found\n", FILE_APPEND);
     }
 
+// 10)
     if(count($parts) == 9 && $parts[2] == "new.arrivalguides.com" && $parts[4] == "Travelguides" && isset($parts[7])){
        $SITE_NAME = $parts[7];
        if(isset($destination[strtoupper($SITE_NAME)])){
